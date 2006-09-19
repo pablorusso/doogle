4ce8f9117de34ff851b692e28023380c80a19a88
pablorusso <pablorusso@732baeec-bf1d-0410-89bf-576031c28093>
2006-09-19 01:18:09 +0000

    Archivos iniciales

---
+++ parser_query_test/src/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include "../../parser_query/includes/QueryParser.h"
+
+using namespace std;
+
+void doTest( string test )
+{
+	vector<Query*> output;
+	vector<Query*>::iterator output_iter;
+
+	cout << "------------------------------------------------------" << endl;
+	cout << "phrase: " << test << endl << endl;
+	QueryParser::Parse( test, output );
+	for (output_iter=output.begin(); output_iter != output.end(); output_iter++)
+	{
+  		cout << "word: " << (*output_iter)->getWord() << endl;
+		cout << "included: " << (*output_iter)->mustBeIncluded() << endl;
+		cout << "excluded: " << (*output_iter)->mustBeExcluded() << endl;
+		cout << "weight: " << (*output_iter)->getWeight() << endl;
+
+		delete *output_iter;
+	}
+	cout << endl << "------------------------------------------------------" << endl;
+}
+
+int main()
+{
+	// validos
+	doTest( "t1 t2 t3" );
+	doTest( "+t1 t2 t3" );
+	doTest( "-t1 t2 t3" );
+	doTest( "t1^2 t2 t3" );
+	doTest( "-t1 +t2 t3^4" );
+
+	// invalidos
+	doTest( "&t1" );
+	doTest( "" );
+	doTest( "++t1" );
+	doTest( "t1^n" );
+	doTest( "t1^^n" );
+	doTest( "t1^^n" );
+}
